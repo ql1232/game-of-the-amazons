ef0c542eff6b2fd5152cc96a91500a725429ff2f
ql1232 <hoobooh48@gmail.com>
2026-02-21 11:26:24 +0900

    init

---
+++ src/main/java/ubc/cosc322/COSC322Test.java
@@ -0,0 +1,122 @@
+
+package ubc.cosc322;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import sfs2x.client.entities.Room;
+import ygraph.ai.smartfox.games.BaseGameGUI;
+import ygraph.ai.smartfox.games.GameClient;
+import ygraph.ai.smartfox.games.GameMessage;
+import ygraph.ai.smartfox.games.GamePlayer;
+import ygraph.ai.smartfox.games.amazons.AmazonsBoard;
+import ygraph.ai.smartfox.games.amazons.AmazonsGameMessage;
+
+/**
+ * An example illustrating how to implement a GamePlayer
+ * @author Yong Gao (yong.gao@ubc.ca)
+ * Jan 5, 2021
+ *
+ */
+public class COSC322Test extends GamePlayer{
+
+    private GameClient gameClient = null; 
+    private BaseGameGUI gamegui = null;
+	
+    private String userName = "cosc322";
+    private String passwd = "cosc322";
+ 	private ArrayList<Integer> gameBoard;
+	
+    /**
+     * The main method
+     * @param args for name and passwd (current, any string would work)
+     */
+    public static void main(String[] args) {				 
+    	COSC322Test player = new COSC322Test("args[2]", "args[1]");
+
+    	if(player.getGameGUI() == null) {
+    		player.Go();
+    	}
+    	else {
+    		BaseGameGUI.sys_setup();
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                	player.Go();
+                }
+            });
+    	}
+    }
+	
+    /**
+     * Any name and passwd 
+     * @param userName
+      * @param passwd
+     */
+    public COSC322Test(String userName, String passwd) {
+    	this.userName = userName;
+    	this.passwd = passwd;
+    	
+    	//To make a GUI-based player, create an instance of BaseGameGUI
+    	//and implement the method getGameGUI() accordingly
+		this.gameBoard=new ArrayList<>();
+		for(int i = 0; i<1000; i++){
+			this.gameBoard.add(0);
+		}
+    	this.gamegui = new BaseGameGUI(this);
+    }
+ 
+
+
+    @Override
+		public void onLogin() {
+		userName = gameClient.getUserName();
+		if(gamegui != null) {
+			gamegui.setRoomInformation(gameClient.getRoomList());
+		}
+	}
+
+    @Override
+    public boolean handleGameMessage(String messageType, Map<String, Object> msgDetails) {
+    	//This method will be called by the GameClient when it receives a game-related message
+    	//from the server.
+	
+    	//For a detailed description of the message types and format, 
+    	//see the method GamePlayer.handleGameMessage() in the game-client-api document.
+		if (messageType.equals(GameMessage.GAME_STATE_BOARD)){
+			this.getGameGUI().setGameState((ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.GAME_STATE));
+		}
+		if (messageType.equals(GameMessage.GAME_ACTION_MOVE)){
+			this.getGameGUI().updateGameState(msgDetails);
+		}
+		System.out.println();
+    	return true;
+    }
+    
+    
+    @Override
+    public String userName() {
+    	return userName;
+    }
+
+	@Override
+	public GameClient getGameClient() {
+		// TODO Auto-generated method stub
+		return this.gameClient;
+	}
+
+	@Override
+	public BaseGameGUI getGameGUI() {
+		// TODO Auto-generated method stub
+		return  this.gamegui;
+	}
+
+	@Override
+	public void connect() {
+		// TODO Auto-generated method stub
+    	gameClient = new GameClient(userName, passwd, this);			
+	}
+
+ 
+}//end of class
