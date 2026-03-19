638920a5244426cd995891c80007b458a48d8a33
RavenHu001 <2419652744@qq.com>
2026-03-18 17:00:39 -0700

    Update HeuristicEvaluator.java

--- src/main/java/ubc/cosc322/HeuristicEvaluator.java
+++ src/main/java/ubc/cosc322/HeuristicEvaluator.java
@@ -137,7 +137,7 @@ public class HeuristicEvaluator {
 
         int mobilityWeight = 2;
         int territoryWeight = 5;
-        return -1 * (mobilityWeight * (mobilitySelf - mobilityOpp) + territoryWeight * territoryScore);
+        return (mobilityWeight * (mobilitySelf - mobilityOpp) + territoryWeight * territoryScore);
     }
 
     /**
